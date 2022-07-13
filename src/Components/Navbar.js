13a39f8b9857ed928ad2cb70377d8e9ec1e0693c
Nurdaulet Melissov <nurdauletktl2019@gmail.com>
2022-07-13 15:18:28 +0600

    init:upload project

---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <nav>
+      <img
+        src="https://dwglogo.com/wp-content/uploads/2017/09/1460px-React_logo.png" alt="logo"
+        className="nav--icon"
+      />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+}
+export default Navbar;
